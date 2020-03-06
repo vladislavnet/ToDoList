4a14eb93e72a9937e6ea5e2cdef56577e58888dc
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-03-06 12:41:36 +0300

    12

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require("./routes");
 const checkRoute = require('./routes/CheckRoute');
+const favicon = require('express-favicon');
 
 let port = process.env.PORT || 5000;
 let app = express();
@@ -11,6 +12,7 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 app.use("/", routes);
 
